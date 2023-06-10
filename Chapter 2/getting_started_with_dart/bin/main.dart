228adf6e7e4187dc2550e51dcd8c5b343a4ad731
Muhammad Usama <imusamairshad@gmail.com>
2023-06-10 13:11:27 +0500

    main.dart is modified.

--- Chapter 2/getting_started_with_dart/bin/main.dart
+++ Chapter 2/getting_started_with_dart/bin/main.dart
@@ -1,5 +1,7 @@
 import 'package:getting_started_with_dart/01-declaring_variables.dart.dart';
+import 'package:getting_started_with_dart/02-strings.dart';
 
 void main() {
   variablePlayground();
+  stringPlayground();
 }
